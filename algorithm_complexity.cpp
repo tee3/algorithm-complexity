ba1ff6e52fa0c0995a54fc34606768ed2ec8eb0d
Thomas Brown <tabsoftwareconsulting@gmail.com>
2015-06-23 17:22:14 -0400

    Preliminary algorithm complexity measurement generalization.

--- algorithm_complexity.cpp
+++ algorithm_complexity.cpp
@@ -32,6 +32,80 @@ operator<< (std::ostream & os, const std::vector<counted_int> & v)
    return os;
 }
 
+/// @brief Returns the averaged counts for the algorithm passed within
+/// the function.
+template<typename Setup, typename Algorithm, typename Container>
+counted_operations
+test_algorithm_complexity (Container c,
+                           Setup setup,
+                           Algorithm algorithm,
+                           int iterations)
+{
+   counted_operations ave;
+   counted_operations min;
+   counted_operations max;
+
+   for (int i = 0; i != iterations; ++i)
+   {
+      setup (c);
+
+      counted_int::reset ();
+
+      algorithm (c);
+
+      counted_operations snapshot = counted_int::counts ();
+
+      ave += snapshot;
+
+      if (snapshot.constructions > max.constructions)
+      {
+         max.constructions = snapshot.constructions;
+      }
+      if (snapshot.assignments > max.assignments)
+      {
+         max.assignments = snapshot.assignments;
+      }
+      if (snapshot.copies > max.copies)
+      {
+         max.copies = snapshot.copies;
+      }
+      if (snapshot.destructions > max.destructions)
+      {
+         max.destructions = snapshot.destructions;
+      }
+      if (snapshot.accesses > max.accesses)
+      {
+         max.accesses = snapshot.accesses;
+      }
+      if (snapshot.comparisons > max.comparisons)
+      {
+         max.comparisons = snapshot.comparisons;
+      }
+      if (snapshot.swaps > max.swaps)
+      {
+         max.swaps = snapshot.swaps;
+      }
+   }
+
+   // calculate averages
+   ave.constructions =
+      std::ceil (ave.constructions / static_cast<long double> (iterations));
+   ave.assignments =
+      std::ceil (ave.assignments / static_cast<long double> (iterations));
+   ave.copies =
+      std::ceil (ave.copies / static_cast<long double> (iterations));
+   ave.destructions =
+      std::ceil (ave.destructions / static_cast<long double> (iterations));
+   ave.accesses =
+      std::ceil (ave.accesses / static_cast<long double> (iterations));
+   ave.comparisons =
+      std::ceil (ave.comparisons / static_cast<long double> (iterations));
+   ave.swaps =
+      std::ceil (ave.swaps / static_cast<long double> (iterations));
+
+   return ave;
+}
+
 BOOST_AUTO_TEST_CASE (algorithm_complexity_all)
 {
    // actual complexity measurement
@@ -67,46 +141,33 @@ BOOST_AUTO_TEST_CASE (algorithm_complexity_all)
                                              O_n_log2_n (v.size ()),
                                              O_n_log2_n (v.size ()));
 
-         counted_operations counts;
+         counted_operations counts =
+            test_algorithm_complexity (v,
+                                       [&] (auto & v)
+                                       {
+                                          std::shuffle (std::begin (v),std::end (v),urng);
+                                       },
+                                       [&] (auto & v)
+                                       {
+                                          std::sort (std::begin (v),std::end (v));
+                                       },
+                                       iterations);
 
-         for (int i = 0; i < iterations; ++i)
-         {
-            std::shuffle (std::begin (v),std::end (v),urng);
-
-            counted_int::reset ();
-
-            std::sort (std::begin (v),std::end (v));
-
-            counts += counted_int::counts ();
-         }
-
-         // calculate averages
-         counts.constructions =
-            std::ceil (counts.constructions /
-                       static_cast<long double> (iterations));
-         counts.assignments =
-            std::ceil (counts.assignments /
-                       static_cast<long double> (iterations));
-         counts.copies =
-            std::ceil (counts.copies /
-                       static_cast<long double> (iterations));
-         counts.destructions =
-            std::ceil (counts.destructions /
-                       static_cast<long double> (iterations));
-         counts.accesses =
-            std::ceil (counts.accesses /
-                       static_cast<long double> (iterations));
-         counts.comparisons =
-            std::ceil (counts.comparisons /
-                       static_cast<long double> (iterations));
-         counts.swaps =
-            std::ceil (counts.swaps /
-                       static_cast<long double> (iterations));
-
-         std::cout << "(v.size, iterations, (n * log_2 (n)): (" << v.size () << ", " << iterations << ")\n";
+         std::cout << "(v.size, iterations): (" << v.size () << ", " << iterations << ")\n";
+         std::cout << "counts:\n";
          std::cout << counts << "\n";
          std::cout << "expected counts:\n";
          std::cout << expected_counts << "\n";
+         std::cout << "complexity estimates\n";
+         std::cout << "(\n";
+         std::cout << "   constructions: " << complexity_string (v.size (),counts.constructions) << "\n";
+         std::cout << "   assignments: " << complexity_string (v.size (),counts.assignments) << "\n";
+         std::cout << "   copies: " << complexity_string (v.size (),counts.copies) << "\n";
+         std::cout << "   destructions: " << complexity_string (v.size (),counts.destructions) << "\n";
+         std::cout << "   accesses: " << complexity_string (v.size (),counts.accesses) << "\n";
+         std::cout << "   comparisons: " << complexity_string (v.size (),counts.comparisons) << "\n";
+         std::cout << "  swaps: " << complexity_string (v.size (),counts.swaps) << "\n";
+         std::cout << ")\n";
 
          // This should succeed on average.
          BOOST_CHECK_EQUAL (counts.constructions,
